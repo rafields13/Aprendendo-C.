6f42c32c7120930bab0c234a56d03d2b85197de1
rafields13 <126572694+rafields13@users.noreply.github.com>
2023-04-05 20:43:39 -0300

    Add files via upload

---
+++ Aula 7.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <locale.h>
+
+    void main () {
+
+        setlocale(LC_ALL, "Portuguese");
+
+        // 1. Faça um código que preencha uma matriz com as quantidades de linha e coluna informadas pelo usuário e que cada elemento seja igual a multiplicação da coordenada da linha pela coordenada coluna.
+
+        int linha, coluna = 0;
+        printf("Digite o número de linhas e de colunas da matriz RESPECTIVAMENTE, por favor:\n");
+        scanf("%d %d", &linha, &coluna);
+
+        int matriz[linha][coluna];
+
+        for (int i = 0; i < linha; i ++) {
+
+            for (int j = 0; j < coluna; j ++) {
+
+                matriz[i][j] = i * j;
+
+            }
+
+        }
+
+        printf("\nMatriz gerada:\n\n");
+
+        for (int i = 0; i < linha; i ++) {
+
+            for (int j = 0; j < coluna; j ++) {
+
+                printf("[%d]", matriz[i][j]);
+
+            }
+
+            printf("\n");
+
+        }
+
+    }
